5b20a079ca67dd39be6345089f0315e89ed60201
sugiyama ryota <sugiyama@anysense.co.jp>
2022-08-20 23:09:20 +0900

    use styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,42 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: applesystem,BlinkMacSystemFont,SegoeUI,Roboto,Oxygen,Ubuntu,Cantarell,FiraSans,DroidSans,HelveticaNeue,sansserif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    cursor: pointer;
+    text-decoration: none;
+    transition: .25s;
+    color: #000;
+  }
+  ol,ul {
+    list-style: none;
+  }
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content='utf-8' />
+        <meta key="viewport" name="viewport" content='width=devicewidth,initialscale=1,shrinktofit=no,maximumscale=5' />
+        <meta property="og:locale"content="ja_JP"/>
+        <meta property="og:type"content="website"/>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
