bd8c381b9f1b63fdc2c25aaf9e1a14751a3a8a1b
sugiyama ryota <sugiyama@anysense.co.jp>
2022-08-21 23:51:01 +0900

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  swcMinify: true,
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    }
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
