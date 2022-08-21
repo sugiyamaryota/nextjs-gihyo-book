2a02bac423cd93951569c6a2db8cb8a68578c371
sugiyama ryota <sugiyama@anysense.co.jp>
2022-08-21 13:25:16 +0900

    customized storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from "@storybook/react"
+import {createGlobalStyle, ThemeProvider } from 'styled-components'
+import {theme} from '../src/themes'
+import * as NextImage from 'next/image'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +12,45 @@ export const parameters = {
     },
   },
 }
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: applesystem,BlinkMacSystemFont,SegoeUI,Roboto,Oxygen,Ubuntu,Cantarell,FiraSans,DroidSans,HelveticaNeue,sansserif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    transition: .25s;
+    color: #000;
+  }
+`
+
+// Themeの適用
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    {story()}
+  </ThemeProvider>
+))
+
+// next/imageの差し替え
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: props => typeof props.src === 'string' ? (
+    <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+  ) : (
+    <OriginalNextImage {...props} unoptimized />
+  ),
+})
+
+Object.defineProperty(NextImage, '__esModule', {
+  configurable: true,
+  value: true
+})
