9dd01ddc5313520818d93f938436b8291bdfd151
sugiyama ryota <sugiyama@anysense.co.jp>
2022-09-14 21:54:57 +0900

    add test

---
+++ src/components/molecules/Dropzone/index.spec.tsx
@@ -0,0 +1,30 @@
+import { render, screen, fireEvent, RenderResult } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import Dropzone from '.'
+import { theme } from 'themes'
+
+describe('Dropzone', () => {
+    let renderResult: RenderResult
+    let handleDrop: jest.Mock
+    
+    beforeEach(() => {
+        handleDrop = jest.fn()
+        renderResult = render(
+            <ThemeProvider theme={theme}>
+                <Dropzone onDrop={handleDrop} />
+            </ThemeProvider>
+        )
+    })
+    afterEach(() => {
+        renderResult.unmount()
+    })
+    it('ファイルがドロップされたらonDropが呼ばれる', async () => {
+        const element = await screen.findByTestId('dropzone')
+        fireEvent.drop(element, {
+            dataTransfer: {
+                files: [new File(['(⌐□_□)'], 'chucknoris.png', {type: 'image/png'})]
+            }
+        })
+        expect(handleDrop).toHaveBeenCalledTimes(1)
+    })
+})
